712b5f2d88b1c97872f876a83403b481c7fb596a
xanimo <dakodagreaves@gmail.com>
2018-02-08 17:56:40 -0800

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,133 @@
+// Data structure that will store the friends data
+var friendList = [
+  {
+    name: 'Mr. Wizard',
+    photo: 'http://sciencenetlinks.com/media/filer_thumbnails/2012/07/10/donherbert.jpg__225x1000_q85.jpg',
+    scores: [
+      '1',
+      '5',
+      '5',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Aleister Crowley',
+    photo: 'http://www.controverscial.com/Aleister_Crowley_young.jpg',
+    scores: [
+      '2',
+      '4',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '2',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Muttley',
+    photo: 'https://i.ytimg.com/vi/9o58ypeFFWk/maxresdefault.jpg',
+    scores: [
+      '4',
+      '3',
+      '5',
+      '1',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1'
+    ]
+  },
+  {
+    name: 'Steven Seagull',
+    photo: 'https://cdni.rt.com/files/2017.05/article/590db183c461881b1c8b45bc.png',
+    scores: [
+      '5',
+      '1',
+      '2',
+      '1',
+      '3',
+      '4',
+      '1',
+      '3',
+      '3',
+      '4'
+    ]
+  },
+  {
+    name: 'Pete Seeger',
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Pete_Seeger11.jpg/210px-Pete_Seeger11.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '1'
+    ]
+  },
+  {
+    name: 'Tony Bologna',
+    photo: 'https://farm4.staticflickr.com/3883/14190402787_4bb53aa625_z.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '1',
+      '3',
+      '2',
+      '5',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Eddie Munster',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5b/Eddie_Munster.JPG/220px-Eddie_Munster.JPG',
+    scores: [
+      '5',
+      '4',
+      '4',
+      '2',
+      '4',
+      '4',
+      '3',
+      '2',
+      '3',
+      '3'
+    ]
+  },
+  {
+    name: 'I like Turtles',
+    photo: 'https://i.ytimg.com/vi/CMNry4PE93Y/hqdefault.jpg',
+    scores: [
+      '5',
+      '2',
+      '3',
+      '1',
+      '5',
+      '1',
+      '3',
+      '1',
+      '1',
+      '5'
+    ]
+  }
+];
+
+module.exports = friendList;
